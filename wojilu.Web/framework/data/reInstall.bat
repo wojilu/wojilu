b499c9e9f21792a53043a36d01ae080d588ac0ca
imneo <imneo54007@gmail.com>
2012-05-11 20:38:17 +0800

    增加 任务功能

---
+++ wojilu.Web/framework/data/reInstall.bat
@@ -0,0 +1,10 @@
+@echo off
+
+del /s /q wojilu.Common.Menus.FooterMenu.config
+del /s /q wojilu.Members.Sites.Domain.SiteApp.config
+del /s /q wojilu.Members.Sites.Domain.SiteMenu.config
+del /s /q wojilu.Web.Controller.Security.AppAdminRole.config
+del /s /q wojilu.Web.Controller.Security.AppRole.config
+
+echo Íê³É
+pause>nul
