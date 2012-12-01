37433e1e15e767753b1501cc24b7d636ffc69c78
shenyuyang <shenyuyang@gmail.com>
2012-12-01 17:57:40 +0800

    框架：增加 SettingBase 简化配置操作

---
+++ wojilu/_wojilu/SettingBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wojilu {
+
+    public class SettingBase<T> {
+
+        public static T Instance {
+            get { return _instance; }
+        }
+
+        private static T _instance = loadSettings();
+
+        private static T loadSettings() {
+            return cfgHelper.Read<T>();
+        }
+
+        public static void Save( T s ) {
+            _instance = s;
+            cfgHelper.Write( _instance );
+        }
+
+    }
+
+}
