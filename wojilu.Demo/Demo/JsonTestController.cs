bb49dd19d3da5d4b64b62c1bb77d38d2a3e79602
shenyuyang <shenyuyang@gmail.com>
2013-03-26 15:52:51 +0800

    界面：继续增加IE6兼容补丁

--- wojilu.Demo/Demo/JsonTestController.cs
+++ wojilu.Demo/Demo/JsonTestController.cs
@@ -17,14 +17,12 @@ namespace wojilu.Web.Controller.Demo {
             // 获取客户端提交的值
             int id = ctx.PostInt( "Id" );
 
+            // 使用dic存放数据，可以避免手工拼接的错误
             Dictionary<String, String> dic = new Dictionary<String, String>();
             dic.Add( "Name", "你选择了" + id );
 
-            // 使用JsonString的Convert方法构造json字符串，可以避免手工拼接的错误
-            String jsonStr = Json.ToString( dic );
-
             // 使用 echoJson 方法返回 json 字符串，客户端jquery可以直接使用
-            echoJson( jsonStr );
+            echoJson( dic );
         }
 
     }
