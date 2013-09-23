cdfb24e8fbf22b03bc2a303afb8bb2a61d2020e9
shenyuyang <shenyuyang@gmail.com>
2013-09-23 10:38:19 +0800

    综合：移除 wojilu.cms 并更名为 wojilu.tutorial

--- wojilu.cms.web/static/js/main.js
+++
@@ -1,9 +0,0 @@
-require(["lib/bootstrap-dropdown"], function() { 
-        
-    $(function() {
-        for( var i=0;i<__funcList.length;i++) {
-             __funcList[i]();
-        }
-    }); 
-
-});
